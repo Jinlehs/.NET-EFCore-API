26400ef5e387874d8accab6bfbfc43fec4e77d5f
Jin Lee <jl6@ualberta.ca>
2022-09-27 16:58:59 -0700

    created fighting functionality

--- DTO/Character/GetCharacterDto.cs
+++ DTO/Character/GetCharacterDto.cs
@@ -19,5 +19,11 @@ namespace dotnet_rpg.DTO.Character
         public GetWeaponDto Weapon { get; set; }
         
         public List<GetSkillDto> Skills { get; set; }
+        
+        public int Fights { get; set; }
+        
+        public int Victories { get; set; }
+        
+        public int Defeats { get; set; }
     }
 }
